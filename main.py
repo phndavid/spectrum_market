86345c35293e69c9f06f50fa0a873af9d3b42deb
N. David Padilla H <phndavid@gmail.com>
2016-10-16 13:10:27 -0500

    dinamyc data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import random
 operador = {"deltas": [
 	{   "id": 1,
 		"cqi": 2,
@@ -29,10 +30,14 @@ def createUser(id, bin_service, time_service):
  return {"user":id, "bin_service":bin_service, "time_service":time_service, "bw": bwByService(time_service)}	
 
 def addUsers(size):
-  users = []
-  for i in xrange(0,size):
-  	bin_service = [1,0,1,0,1]
-  	time_service = [3,0,5,0,2]
+  users = []  
+  for i in xrange(1,size+1):
+  	bin_service = [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1)]
+  	time_service = [0,0,0,0,0]
+  	for i in xrange(1,5):
+  		if bin_service[i] == 1:
+  			time_service[i] = random.randint(1,20)
+  	
   	users.append(createUser(i,bin_service, time_service))
   return users;
 
@@ -44,7 +49,13 @@ def bwByService(array):
 		bw.append(array[i] * services[i])
 	return bw
 
-operador["deltas"].append(delta(10,10, addUsers(2)))
+def addDeltas(size):
+	for i in xrange(1,size+1):		
+		cqi = random.randint(1, 15)
+		num_users = random.randint(1, 5);
+		operador["deltas"].append(delta(i,cqi, addUsers(num_users)))
+
+addDeltas(2)
 #Nos devuelve el String con el JSON
 data_string = json.dumps(operador)
 for key,value in operador.iteritems():
