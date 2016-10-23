3728ccd7e125a2a92fd0c1979311467e87df0e97
N. David Padilla H <phndavid@gmail.com>
2016-10-23 11:16:44 -0500

    documentar codigo

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ Ejemplo del formato Json a utilizar.
 """
 operador = {"deltas": [
 	{   "id": 1,
+		"escala_t": 1,
 		"cqi": 2,
 		"users":[
 		   {"user":1, "bin_service":[1,0,1,0,1], "time_service":[2,0,4,0,3], "bw":[1,0,1,0,1], "sum_bw": 3},
@@ -33,11 +34,12 @@ Metodo: createUser
 Descripción: crea un objeto usuario de acuerdo a los parametros pasados. 
 Parametros:
 	- id:
-	- bin_service:
-	- time_service: 
+	- cqi: calidad del canal
+	- bin_service: binaria por servicio donde 0: no hace uso del servicio 1: hace uso del servicio
+	- time_service:  cantidad de tiempo por servicio
 """
-def createUser(id, bin_service, time_service): 
-	bw = bwByService(time_service)
+def createUser(id, cqi, bin_service, time_service): 
+	bw = bwByService(cqi, time_service)
 	sum_bw = sumBw(bw)
 	user = {"user":id, "bin_service":bin_service, "time_service":time_service, "bw": bw, "sum_bw": sum_bw}	
 	return user
@@ -46,32 +48,36 @@ def createUser(id, bin_service, time_service):
 Metodo: addUsers
 Descripción: crea un array con un número de usuarios n. 
 Parametros:
+	- cqi: calidad del canal
 	- size: número de usuarios a crear. 
+	- escala_t: escala en el tiempo (ej. 1min, 10min, 60min)
 """
-def addUsers(size):
+def addUsers(cqi, size, escala_t):
   users = []  
-  for i in xrange(1,size+1):
+  for i in xrange(1,(size+1)*escala_t):
   	bin_service = [random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1),random.randint(0,1)]
   	time_service = [0,0,0,0,0]
   	for i in xrange(1,5):
   		if bin_service[i] == 1:
   			time_service[i] = random.randint(1,20)
   	
-  	users.append(createUser(i,bin_service, time_service))
+  	users.append(createUser(i,cqi, bin_service, time_service))
   return users;
 
 """
+Ecuación: 13
 Metodo: bwByService
 Descripción: calcula el ancho de banda por serivicio 
 Parametros:
-	- array:
+	- cqi: calidad del canal
+	- array: lista de cantidades de tiempo por servicio.
 """
-def bwByService(array):
+def bwByService(cqi, array):
 	#ftp, web, video, voip, games
 	bw = []
  	services =[467.20, 338.39, 53.02, 21.42, 21.42]
 	for i in xrange(0,5):
-		bwi = array[i] * services[i];
+		bwi = (services[i]/cqi)*array[i];
 		bw.append(bwi)	
 	return bw
 
@@ -79,7 +85,7 @@ def bwByService(array):
 Metodo: sumBw
 Descripción: calcula la suma total del ancho de banda por serivicio de un usuario.
 Parametros:
-	- array:
+	- array: lista de anchos de banda por servicio
 """
 def sumBw(array):
 	sumBw = 0;
@@ -88,10 +94,11 @@ def sumBw(array):
 	return sumBw
 
 """
+Ecuación: 14
 Metodo: sumBw
 Descripción: calcula la suma total del ancho de banda por delta de t.
 Parametros:
-	- users:
+	- users: lista de usuarios en un delta t
 """
 def totallyBW(users):
 	totallyBW = 0
@@ -101,10 +108,11 @@ def totallyBW(users):
 	return totallyBW
 
 """
+Ecuación: 15
 Metodo: sumBw
 Descripción: calcula el ancho de banda  maximo por delta de t.
 Parametros:
-	- users:
+	- users: lista de usuarios en un delta t
 """
 def maxBW(users):
 	maxBW = 0
@@ -119,12 +127,14 @@ Metodo: delta
 Descripción: crea un objeto delta de acuerdo a los parametros pasados. 
 Parametros:
 	- id:
-	- cqi:
-	- users: 
+	- escala_t: escala en el tiempo (ej. 1min, 10min, 60min)
+	- cqi: calidad del canal [1,15]
+	- users: lista de usuarios por delta de t
 """
-def delta(id, cqi, users):
+def delta(id, escala_t, cqi, users):
 	delta  = {
 		"id": id,
+		"escala_t": escala_t,
 		"cqi": cqi,
 		"users": users,
 		"totallyBW": totallyBW(users),
@@ -136,19 +146,20 @@ def delta(id, cqi, users):
 Metodo: sumBw
 Descripción: agrega un numero de deltas n a un operador
 Parametros:
+	- escala_t: escala en el tiempo (ej. 1min, 10min, 60min)
 	- size: número de deltas.
 	- size_users: número de usuarios por delta t.
 """
-def addDeltas(size, size_users):
+def addDeltas(escala_t, size, size_users):
 	for i in xrange(1,size+1):		
 		cqi = random.randint(1, 15)
 		num_users = random.randint(1, size_users);
-		operador["deltas"].append(delta(i,cqi, addUsers(num_users)))
+		operador["deltas"].append(delta(i,escala_t, cqi, addUsers(cqi, num_users, escala_t)))
 
 """
 Permite crear todos los deltas t que tendrá un operador.
 """
-addDeltas(2,60);
+addDeltas(1,2,60);
 
 """
 Genera archivo en formato json con todos los datos del operador
